eeaa62d25d125f3e970015679445f49aebb23842
datsfilipe <datsfilipe.foss@proton.me>
2023-11-18 13:41:08 -0300

    test: update tests

--- test/webrtc_server_test.exs
+++ test/webrtc_server_test.exs
@@ -1,8 +1,15 @@
 defmodule WebrtcServerTest do
   use ExUnit.Case
-  doctest WebrtcServer
 
-  test "greets the world" do
-    assert WebrtcServer.hello() == :world
+  test "child_spec/1" do
+    # not gonna test now
+  end
+
+  test "handle request '/'" do
+    # not gonna test now
+  end
+
+  test "handle request not matching '/'" do
+    # not gonna test now
   end
 end
