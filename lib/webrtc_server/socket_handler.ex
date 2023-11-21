5d43fbc0d5a5f431f7e81cec493b742bd7d2c0f0
datsfilipe <datsfilipe.foss@proton.me>
2023-11-21 02:08:25 -0300

    refactor: update socket handler with new events

--- lib/webrtc_server/socket_handler.ex
+++ lib/webrtc_server/socket_handler.ex
@@ -1,4 +1,5 @@
 defmodule WebrtcServer.SocketHandler do
+  import Jason
   @behaviour :cowboy_websocket
 
   def init(req, state) do
@@ -10,20 +11,34 @@ defmodule WebrtcServer.SocketHandler do
     {:ok, state}
   end
 
-  def websocket_handle({:text, "ping"}, state) do
-    {:reply, {:text, "pong"}, state}
-  end
-
-  def websocket_handle({:text, _}, state) do
-    {:reply, {:text, "Not implemented"}, state}
+  def websocket_handle({:text, data}, state) do
+    case WebrtcServer.Room.match(data) do
+      {:ok, %{room_id: room_id}} ->
+        {:reply, {:text, encode!(%{"room_id" => room_id})}, state}
+      {:invalid_message} ->
+        {:reply, {:text, encode!(%{"error" => "invalid message"})}, state}
+      {:room_already_exists} ->
+        {:reply, {:text, encode!(%{"error" => "room already exists"})}, state}
+      {:room_has_no_users} ->
+        {:reply, {:text, encode!(%{"error" => "room has no users"})}, state}
+      {:user_already_joined} ->
+        {:reply, {:text, encode!(%{"error" => "user already joined"})}, state}
+      {:room_not_found} ->
+        {:reply, {:text, encode!(%{"error" => "room not found"})}, state}
+      {:user_not_joined} ->
+        {:reply, {:text, encode!(%{"error" => "user not joined"})}, state}
+      _ ->
+        {:reply, {:text, encode!(%{"error" => "message does not match any pattern"})}, state}
+    end
   end
 
   def websocket_info(info, state) do
-    IO.inspect info
+    IO.puts("Websocket info: #{inspect(info)}")
     {:ok, state}
   end
 
-  def terminate(_reason, _req, _state) do
+  def terminate(reason, _req, _state) do
+    IO.puts("Terminating websocket: #{inspect(reason)}")
     :ok
   end
 end
